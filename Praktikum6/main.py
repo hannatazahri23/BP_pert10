3ea789bea15fa252135f4fa53219ee56873f62b6
hannatazahri23 <hannatadjava23@gmail.com>
2022-11-28 20:38:30 +0700

    KERJAIN...!!!

---
+++ Praktikum6/main.py
@@ -0,0 +1,16 @@
+# dict = {
+#     "Player" : "Albert", 
+#     "Position" : "Jungler",
+#     "Squad" : "RRQ"
+# }
+
+# print("dict [Player] = ", dict["Player"])
+# print("dict [Position] = ", dict["Position"])
+# print(f"dict [Squad] = {dict['Squad']}")
+
+kontak = {
+    "Nama" : "Hannata",
+    "Nomor" : "081210941107"
+}
+
+print("|" + "Nama".center(8) + "|")
